8917c9575e4664ae79cbcac061b6f7a0a0b04196
sanyalsc <sanyalster@gmail.com>
2023-11-25 15:57:52 -0800

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import setuptools
 
-setuptools.setup(name='var-next',
+setuptools.setup(name='varnext',
       version='0.1',
       description='The funniest joke in the world',
       url='https://github.com/sanyalsc/var-Next',
       author='Shantanu Sanyal',
       author_email='sanyalster@gmail.com',
       license='MIT',
-      packages=['src/var_next'],
+      packages=['var_next'],
+      package_dir={'':'src'}
       install_requires=[
       ])
+
